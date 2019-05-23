fce6dec4201fa2f201bae761bc08ba631c78a061
Richard Hull <rm_hull@yahoo.co.uk>
2019-05-23 12:39:50 +0100

    Wording

--- tr4_monitor/hotspot/cpu_stats.py
+++ tr4_monitor/hotspot/cpu_stats.py
@@ -14,7 +14,7 @@ def render(draw, width, height):
     stats = psutil.cpu_stats()
     draw.text((10, 0), 'Ctx switch', fill='white', font=default)
     right_text(draw, width, 0, text=str(stats.ctx_switches), font=default)
-    draw.text((10, 10), 'Intr', fill='white', font=default)
+    draw.text((10, 10), 'Interrupts', fill='white', font=default)
     right_text(draw, width, 10, text=str(stats.interrupts), font=default)
     draw.text((10, 20), 'Soft intr', fill='white', font=default)
     right_text(draw, width, 20, text=str(stats.soft_interrupts), font=default)
