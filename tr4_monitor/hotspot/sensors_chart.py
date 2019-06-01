efd95c95e440633a3fb4046d75a1baa4e51b96d0
Richard Hull <rm_hull@yahoo.co.uk>
2019-06-01 10:03:13 +0100

    Adjust vertical text position

--- tr4_monitor/hotspot/sensors_chart.py
+++ tr4_monitor/hotspot/sensors_chart.py
@@ -11,14 +11,14 @@ def using(datalogger, spec):
         snapshot = list(datalogger.entries)
         y_offset = 0
         height = 12
-        
+
         for label, reading in spec.items():
             max_temp = max(snapshot, key=lambda x: x['value'][reading])
             latest_temp = int(snapshot[-1]['value'][reading])
-            
-            draw.text((0, y_offset + 2), label, fill='white', font=default)
-            right_text(draw, width / 2 + 8, y_offset + 2, text=f'{latest_temp}°C', font=default)
-            
+
+            draw.text((0, y_offset + 1), label, fill='white', font=default)
+            right_text(draw, width / 2 + 8, y_offset + 1, text=f'{latest_temp}°C', font=default)
+
             for index, entry in enumerate(reversed(snapshot)):
                 x_offset = width - index - 2
                 y_value = int((height - 2) * entry['value'][reading] / max_temp['value'][reading])
@@ -27,5 +27,5 @@ def using(datalogger, spec):
             draw.rectangle([width - datalogger.max_entries - 2, y_offset, width - 2, y_offset + height - 2], outline='white', width=1)
 
             y_offset += height
-            
+
     return render
