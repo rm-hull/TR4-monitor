a267927df10d472115d1856e028637e0cda0b0cc
Richard Hull <rm_hull@yahoo.co.uk>
2019-06-05 00:27:21 +0100

    Be tolerant of non-existant readings

--- tr4_monitor/hotspot/sensors_chart.py
+++ tr4_monitor/hotspot/sensors_chart.py
@@ -13,23 +13,25 @@ def using(datalogger, temp_spec, fan_spec):
         height = 12
 
         for label, reading in temp_spec.items():
-            max_temp = max(snapshot, key=lambda x: x['value'][reading])
-            latest_temp = int(snapshot[-1]['value'][reading])
+            max_temp = max(snapshot, key=lambda x: x['value'].get(reading, 0))
+            latest_temp = snapshot[-1]['value'].get(reading)
+            latest_temp = 'n/a' if latest_temp is None else int(latest_temp)
 
             draw.text((0, y_offset + 1), label, fill='white', font=default)
             right_text(draw, width / 2 + 8, y_offset + 1, text=f'{latest_temp}°C', font=default)
 
-            for index, entry in enumerate(reversed(snapshot)):
-                x_offset = width - index - 2
-                y_value = int((height - 2) * entry['value'][reading] / max_temp['value'][reading])
-                draw.line([x_offset, y_offset + height - 2, x_offset, y_offset + height - y_value], fill='grey', width=1)
+            if reading in max_temp['value']:
+                for index, entry in enumerate(reversed(snapshot)):
+                    x_offset = width - index - 2
+                    y_value = int((height - 2) * entry['value'][reading] / max_temp['value'][reading])
+                    draw.line([x_offset, y_offset + height - 2, x_offset, y_offset + height - y_value], fill='grey', width=1)
 
             draw.rectangle([width - datalogger.max_entries - 2, y_offset, width - 2, y_offset + height - 2], outline='white', width=1)
             y_offset += height
 
-        draw.text((0, y_offset + 1), 'Fans', fill='white', font=default) 
+        draw.text((0, y_offset + 1), 'Fans', fill='white', font=default)
         for index, (label, reading) in enumerate(fan_spec.items()):
-            latest_speed = int(snapshot[-1]['value'][reading])
+            latest_speed = int(snapshot[-1]['value'].get(reading, 0))
             right_text(draw, 56 + (index * 24), y_offset + 1, text=f'{latest_speed}', font=default)
 
     return render
