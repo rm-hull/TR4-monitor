aec9ee4f77ab41c246da98536844335e5c9949d8
Richard Hull <rm_hull@yahoo.co.uk>
2019-06-15 22:24:45 +0100

    Normalize temperatures so all charts have same max value

--- tr4_monitor/hotspot/sensors_chart.py
+++ tr4_monitor/hotspot/sensors_chart.py
@@ -4,6 +4,16 @@
 
 from common import right_text
 from fonts import default
+from nice import nice_ticks
+
+
+def get_temperature_scale(snapshot, temp_spec):
+    readings = []
+    for _, reading in temp_spec.items():
+        readings += [x['value'].get(reading, 0) for x in snapshot]
+
+    max_temp = max(readings)
+    return nice_ticks(0, max_temp)
 
 
 def using(datalogger, temp_spec, fan_spec):
@@ -11,20 +21,20 @@ def using(datalogger, temp_spec, fan_spec):
         snapshot = list(datalogger.entries)
         y_offset = 0
         height = 12
+        
+        min_temp, max_temp, _ = get_temperature_scale(snapshot, temp_spec)
 
         for label, reading in temp_spec.items():
-            max_temp = max(snapshot, key=lambda x: x['value'].get(reading, 0))
             latest_temp = snapshot[-1]['value'].get(reading)
             latest_temp = 'n/a' if latest_temp is None else int(latest_temp)
 
             draw.text((0, y_offset + 1), label, fill='white', font=default)
             right_text(draw, width / 2 + 8, y_offset + 1, text=f'{latest_temp}°C', font=default)
 
-            if reading in max_temp['value']:
-                for index, entry in enumerate(reversed(snapshot)):
-                    x_offset = width - index - 2
-                    y_value = int((height - 2) * entry['value'][reading] / max_temp['value'][reading])
-                    draw.line([x_offset, y_offset + height - 2, x_offset, y_offset + height - y_value], fill='grey', width=1)
+            for index, entry in enumerate(reversed(snapshot)):
+                x_offset = width - index - 2
+                y_value = int((height - 2) * entry['value'].get(reading, 0) / max_temp)
+                draw.line([x_offset, y_offset + height - 2, x_offset, y_offset + height - y_value], fill='grey', width=1)
 
             draw.rectangle([width - datalogger.max_entries - 2, y_offset, width - 2, y_offset + height - 2], outline='white', width=1)
             y_offset += height
---
+++ tr4_monitor/nice.py
@@ -0,0 +1,118 @@
+# Copyright 2008-2016 by Carnegie Mellon University
+# See license information in LICENSE-OPENSOURCE.txt
+
+"""
+A set of functions to produce ranges of aesthetically-pleasing numbers
+that have the specified length and include the specified range.
+Functions are provided for producing nice numeric and time-based
+ranges.
+"""
+
+from __future__ import division
+import math
+
+#
+# Regular number stuff (code mutated from original work by Katherine
+# Prevost)
+#
+
+
+nice_intervals = [1.0, 2.0, 2.5, 3.0, 5.0, 10.0]
+int_intervals = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 8.0, 10.0]
+int_12_intervals = [1.0, 2.0, 3.0, 4.0, 6.0, 12.0]
+int_60_intervals = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 10.0,
+                    12.0, 15.0, 20.0, 30.0]
+
+
+def nice_ceil(x, intervals=nice_intervals, base=10.0):
+    if x == 0:
+        return 0
+    if x < 0:
+        return nice_floor(x * -1, intervals, base) * -1
+    z = base ** math.floor(math.log(x, base))
+    for i in range(len(intervals) - 1):
+        result = intervals[i] * z
+        if x <= result: 
+            return result
+    return intervals[-1] * z
+
+
+def nice_floor(x, intervals=nice_intervals, base=10.0):
+    if x == 0:
+        return 0
+    if x < 0:
+        return nice_ceil(x * -1, intervals, base) * -1
+    z = base ** (math.ceil(math.log(x, base)) - 1.0)
+    for i in range(len(intervals) - 1, 1, -1):
+        result = intervals[i] * z
+        if x >= result:
+            return result
+    return intervals[0] * z
+
+
+def nice_round(x, intervals=nice_intervals, base=10.0):
+    if x == 0:
+        return 0
+    z = base ** (math.ceil(math.log(x, base)) - 1.0)
+    for i in range(len(intervals) - 1):
+        result = intervals[i] * z
+        cutoff = (result + intervals[i + 1] * z) / 2.0
+        if x <= cutoff:
+            return result
+    return intervals[-1] * z
+
+
+def nice_ticks(lo, hi, ticks=5, inside=False,
+               intervals=nice_intervals, base=10.0):
+    """
+    Find 'nice' places to put *ticks* tick marks for numeric data
+    spanning from *lo* to *hi*.  If *inside* is ``True``, then the
+    nice range will be contained within the input range.  If *inside*
+    is ``False``, then the nice range will contain the input range.
+    To find nice numbers for time data, use :func:`nice_time_ticks`.
+
+    The result is a tuple containing the minimum value of the nice
+    range, the maximum value of the nice range, and an iterator over
+    the tick marks.
+
+    See also :func:`nice_ticks_seq`.
+    """
+
+    if lo > hi:
+        value_error = ValueError(
+            "Low value greater than high value: %r, %r" % (lo, hi))
+        raise value_error
+
+    delta_x = hi - lo
+    if delta_x == 0:
+        lo = nice_floor(lo, intervals, base)
+        hi = nice_ceil(hi, intervals, base)
+        delta_x = hi - lo
+        if delta_x == 0:
+            lo = lo - 0.5
+            hi = hi + 0.5
+            delta_x = hi - lo
+
+    delta_t = nice_round(delta_x / (ticks - 1), intervals, base)
+    if inside:
+        lo_t = math.ceil(lo / delta_t) * delta_t
+        hi_t = math.floor(hi / delta_t) * delta_t
+    else:
+        lo_t = math.floor(lo / delta_t) * delta_t
+        hi_t = math.ceil(hi / delta_t) * delta_t
+
+    def t_iter():
+        t = lo_t
+        while t <= hi_t:
+            yield t
+            t = t + delta_t
+
+    return (lo_t, hi_t, t_iter())
+
+
+def nice_ticks_seq(lo, hi, ticks=5, inside=False):
+    """
+    A convenience wrapper of :func:`nice_ticks` to return the nice
+    range as a sequence.
+    """
+    return tuple(nice_ticks(lo, hi, ticks, inside)[2])
