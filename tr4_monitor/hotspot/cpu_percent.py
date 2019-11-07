d1a28aa364548067befb5e7560ec64bc9bd9820f
Richard Hull <rm_hull@yahoo.co.uk>
2019-11-07 20:40:37 +0000

    Show min/max CPU frequency

--- tr4_monitor/hotspot/cpu_percent.py
+++ tr4_monitor/hotspot/cpu_percent.py
@@ -8,9 +8,16 @@ from common import right_text
 from fonts import default
 
 
+def _get_current(reading):
+    return reading.current
+
+
 def render(draw, width, height):
     percent = psutil.cpu_percent()
-    freq = psutil.cpu_freq().current
+    cpu_freqs = psutil.cpu_freq(percpu=True)
+    max_freq = max(cpu_freqs, key=_get_current).current
+    min_freq = min(cpu_freqs, key=_get_current).current
+    
     draw.text((0, 0), 'CPU', fill='white', font=default)
-    right_text(draw, width - 48, 0, text=f'{freq:.0f} MHz', fill='white', font=default)
-    right_text(draw, width, 0, text=f'{percent:.2f}%', font=default)
+    right_text(draw, width - 32, 0, text=f'{min_freq:.0f}/{max_freq:.0f} MHz', fill='white', font=default)
+    right_text(draw, width, 0, text=f'{percent:.1f}%', font=default)
